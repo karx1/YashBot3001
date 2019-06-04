9dfb77c9c4828a7f8b143fc26483f07fa0c8db18
nerdstep710 <49726028+nerdstep710@users.noreply.github.com>
2019-06-04 14:28:12 -0500

    new github repo!

---
+++ main.py
@@ -0,0 +1,50 @@
+from discord.ext.commands import Bot
+from keep_alive import keep_alive
+import os
+import discord
+
+
+
+
+BOT_PREFIX = (";")
+client = Bot(command_prefix=BOT_PREFIX, case_insensitive=True)
+commands = discord.ext.commands
+
+client.remove_command('help')
+
+
+extensions = [
+  'jishaku',
+  'cogs.fun',
+  'cogs.math',
+  'cogs.text',
+  'cogs.yts',
+  'cogs.web',
+  'cogs.other',
+  'cogs.owner',
+  'cogs.events',
+  'cogs.info',
+  'cogs.image',
+]
+
+if __name__ == '__main__':
+  for e in extensions:
+    client.load_extension(e)
+
+
+#the discord game activity
+@client.event
+async def on_ready():
+  user_count = len(client.users)
+  server_count = len(client.guilds)
+  await client.change_presence(activity=discord.Game(name='{} users | ;help'.format(user_count, server_count)))
+  print("Existing Servers:")
+  async for guild in client.fetch_guilds():
+    print(guild.name)
+
+
+#initialises the bot
+keep_alive()
+#the token is stored in an .env file. If you fork this, you have to recreate that with the token inside
+code = os.environ.get("BOT_TOKEN")
+client.run(code)
