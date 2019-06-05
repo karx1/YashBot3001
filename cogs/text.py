5260ffcf4d2834b21686f1e4f2382d98089dae1b
nerdstep710 <49726028+nerdstep710@users.noreply.github.com>
2019-06-04 21:45:48 -0500

    Update text.py

--- cogs/text.py
+++ cogs/text.py
@@ -34,40 +34,30 @@ class TextCog(commands.Cog):
     await ctx.send(message)
 
   @commands.command()
-  async def tts(self, ctx, *, message=""):
-    if message is "":
-      message = ctx.message.author.display_name
+  async def tts(self, ctx, *, message: commands.clean_content):
     await ctx.trigger_typing()
     tts = gTTS(text=message.lower(), lang="en")
     tts.save('cogs/data/out/out.wav')
     await ctx.send(file=discord.File('cogs/data/out/out.wav'))
 
   @commands.command()
-  async def ascii(self, ctx, *, inp=""):
-    if inp == "":
-      inp = ctx.message.author.display_name
+  async def ascii(self, ctx, *, inp: commands.clean_content):
     ascii_banner = figlet_format(inp, font="small")
     await ctx.send(f"```{ascii_banner}```")
   
 
   @commands.command()
-  async def upper(self, ctx, *, message=""):
-    if message == "":
-      message = ctx.message.author.display_name
+  async def upper(self, ctx, *, message: commands.clean_content):
     message = message.upper()
     await ctx.send(message)
 
   @commands.command()
-  async def lower(self, ctx, *, message=""):
-    if message == "":
-      message = ctx.message.author.display_name
+  async def lower(self, ctx, *, message: commands.clean_content):
     message = message.lower()
     await ctx.send(message)
 
   @commands.command()
-  async def reverse(self, ctx, *, message=""):
-    if message == "":
-      message = ctx.message.author.name
+  async def reverse(self, ctx, *, message: commands.clean_content):
     message = message[::-1]
     await ctx.send(message)
 
