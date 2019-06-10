27505c5d6bec7a8dd938f51565477bd528327916
nerdstep710 <49726028+nerdstep710@users.noreply.github.com>
2019-06-10 15:01:11 -0500

    Update web.py

--- cogs/web.py
+++ cogs/web.py
@@ -3,29 +3,40 @@ from discord.ext import commands
 from googlesearch import search
 import urllib.request
 from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor
 
 class WebCog(commands.Cog):
   def __init__(self, client):
     self.client = client
 
-  @commands.command()
-  async def google(self, ctx, *, query=""):
-    name = str(ctx.message.author)
-    name_plain = ctx.message.author.display_name
-    if query is "":
-      await ctx.send(f"You must provide a search term, {name_plain}!")
-      return
-    await ctx.trigger_typing()
-    for j in search(query, tld="com", num=1, stop=1):
-      print(f"{name} has searched for '{query}' and it returned {j}")
-      await ctx.send(j)
+
+
+
 
   @commands.command()
-  async def youtube(self, ctx, *, query=""):
-    r = 0
-    if query == "":
-      await ctx.send(f"You must provide a search term, {ctx.author.mention}!")
+  async def google(self, ctx, *, q=""):
+    def gsync(query=q):
+      name = str(ctx.message.author)
+      name_plain = ctx.message.author.display_name
+      if query is "":
+        return f"You must provide a search term, {name_plain}!"
+      elif query in ["thanos", "Thanos"]:
+        query = "who is thanos"
+      for j in search(query, tld="com", num=1, stop=1):
+        print(f"{name} has searched for '{query}' and it returned {j}")
+        return j
     async with ctx.typing():
+      gasync = await self.client.loop.run_in_executor(ThreadPoolExecutor(), gsync)
+      await ctx.send(gasync)
+
+      
+
+  @commands.command()
+  async def youtube(self, ctx, *, q=""):
+    def ytsync(query=q):
+      r = 0
+      if query == "":
+        return f"You must provide a search term, {ctx.author.mention}!"
       search = urllib.parse.quote(query)
       url = f"https://www.youtube.com/results?search_query={search}"
       response = urllib.request.urlopen(url)
@@ -33,8 +44,11 @@ class WebCog(commands.Cog):
       soup = BeautifulSoup(html, 'html.parser')
       for vid in soup.findAll(attrs={'class':'yt-uix-tile-link'}):
         while r == 0:
-          await ctx.send(f"https://www.youtube.com{vid['href']}")
+          return f"https://www.youtube.com{vid['href']}"
           r = r + 1
+    async with ctx.typing():
+      ytasync = await self.client.loop.run_in_executor(ThreadPoolExecutor(), ytsync)
+      await ctx.send(ytasync)
 
 def setup(client):
   client.add_cog(WebCog(client))
