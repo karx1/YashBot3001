71dde9a1b7a559294adbb11d80327b13ea95f0c4
nerdstep710 <49726028+nerdstep710@users.noreply.github.com>
2019-06-20 16:06:58 -0500

    New tag commands - tagsearch and raw

--- cogs/tags.py
+++ cogs/tags.py
@@ -7,6 +7,7 @@ from whoosh.fields import Schema, STORED, ID, KEYWORD, TEXT
 from whoosh.query import *
 from whoosh.query import Every
 from whoosh.qparser import QueryParser
+from whoosh.writing import AsyncWriter
 
 
 
@@ -25,8 +26,9 @@ class Tags(commands.Cog):
       ix = open_dir("index")
     else:
       ix = open_dir("index")
-    writer = ix.writer()
-    writer.update_document(title=name, content=content, path=name, editor=str(ctx.author))
+    writer = AsyncWriter(ix)
+    writer.update_document(title=name, content=content, path=name, editor=str(ctx.message.author))
+
     writer.commit()
     await ctx.send(f"Created tag {name}")
   
@@ -56,7 +58,7 @@ class Tags(commands.Cog):
       ix = open_dir("index")
     else:
       ix = open_dir("index")
-    writer = ix.writer()
+    writer = AsyncWriter(ix)
     writer.update_document(title=name, content=content, path=name, editor=str(ctx.author))
     writer.commit()
     await ctx.send(f"Edited tag {name}")
@@ -89,7 +91,7 @@ class Tags(commands.Cog):
       ix = open_dir('index')
     else:
       ix = open_dir("index")
-    writer = ix.writer()
+    writer = AsyncWriter(ix)
     writer.delete_by_term('path', name)
     writer.commit()
     await ctx.send(f"Deleted tag {name}")
@@ -107,7 +109,7 @@ class Tags(commands.Cog):
       query = parser.parse(name)
       corrected = searcher.correct_query(query, name, maxdist=2)
       if corrected.query != query:
-        await ctx.send(f"Tag not found. Did you mean {corrected.string}?")
+        await ctx.send(f"Tag not found. Did you mean: {corrected.string}?")
       else:
         results = searcher.search(query)
         title = results[0]['title']
@@ -116,6 +118,79 @@ class Tags(commands.Cog):
         embed.add_field(name="Title", value=title)
         embed.add_field(name="Last edited by", value=editor, inline=False)
         await ctx.send(embed=embed)
+
+  @commands.command()
+  async def tagsearch(self, ctx, *, name):
+    if not os.path.exists('index'):
+      os.mkdir('index')
+      ix = create_in('index', self.schema)
+      ix = open_dir('index')
+    else:
+      ix = open_dir('index')
+    with ix.searcher() as searcher:
+      parser = QueryParser('title', schema=ix.schema)
+      query = parser.parse(name)
+      results = searcher.search(query)
+      corrected = searcher.correct_query(query, name, maxdist=2)
+      if corrected.query != query:
+        await ctx.send(f"Tag not found. Did you mean: {corrected.string}?")
+      else:      
+        x = []
+        i = 0
+        for result in results:
+          i += 1
+          title = result['title']
+          x.append(f"{1}. {title}")
+        y = "\n".join(x)
+        embed = discord.Embed(title=f"Results for {name}:", description=y, color=0x00ff00)
+        await ctx.send(embed=embed)
+
+  @commands.command()
+  async def raw(self, ctx, *, name):
+    if not os.path.exists('index'):
+      os.mkdir('index')
+      ix = create_in('index', self.schema)
+      ix = open_dir('index')
+    else:
+      ix = open_dir('index')
+    with ix.searcher() as searcher:
+      parser = QueryParser('title', ix.schema)
+      query = parser.parse(name)
+      corrected = searcher.correct_query(query, name, maxdist=2)
+      if corrected.query != query:
+        await ctx.send(f"Tag not found. Did you mean: {corrected.string}?")
+      else:
+        results = searcher.search(query)
+        cleaned = discord.utils.escape_markdown(results[0]['content'])
+        await ctx.send(cleaned)
+
+  @commands.command()
+  async def create(self, ctx):
+    await ctx.send(f"Hey! So I heard you want to make a tag. What's it gonna be called? Type your answer in the chat, or type {ctx.prefix}abort at any to stop making a tag.")
+    def check(m):
+      return m.channel == ctx.message.channel and m.author == ctx.message.author
+    msg = await self.client.wait_for('message', check=check)
+    if msg.content == f"{ctx.prefix}abort":
+      await ctx.send("Stopping tag creation.") 
+    else:
+      title = msg.content
+      await ctx.send(f"Cool! The name is {title}. What about the content? Type your answer in the chat.")
+      msg = await self.client.wait_for('message', check=check)
+      if msg.content == f"{ctx.prefix}abort":
+        await ctx.send("Stopping tag creation.")
+      else:
+        content = msg.content
+        if not os.path.exists('index'):
+          os.mkdir('index')
+          ix = create_in('index', schema=self.schema)
+          ix = open_dir('index')
+        else:
+          ix = open_dir('index')
+        writer = AsyncWriter(ix)
+        writer.update_document(title=title, content=content, path=title, editor=str(ctx.message.author))
+        writer.commit()
+        await ctx.send(f"Created tag {title}")
+    
 
 def setup(client):
   client.add_cog(Tags(client))
