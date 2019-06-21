ed21e51c5780a09d3389cd1133c1001f39ab0151
nerdstep710 <49726028+nerdstep710@users.noreply.github.com>
2019-06-21 11:50:08 -0500

    Update tags.py

--- cogs/tags.py
+++ cogs/tags.py
@@ -4,10 +4,7 @@ import os.path
 import os
 from whoosh.index import create_in, open_dir
 from whoosh.fields import Schema, STORED, ID, KEYWORD, TEXT
-from whoosh.query import *
-from whoosh.query import Every
-from whoosh.qparser import QueryParser
-from whoosh.writing import AsyncWriter
+import dataset
 
 
 
@@ -15,154 +12,51 @@ class Tags(commands.Cog):
   def __init__(self, client):
     self.client = client
     self.schema = Schema(title=TEXT(stored=True), content=TEXT(stored=True), path=ID(unique=True), tags=KEYWORD, icon=STORED, editor=TEXT(stored=True))
+    self.db = dataset.connect('	postgres://rnoxyawx:C-JNS12hplxKvRoZz2IQSJbphtUy1SIF@hansken.db.elephantsql.com:5432/rnoxyawx')
+    self.table = self.db['tags']
   
 
 
   @commands.command()
   async def make(self, ctx, name, *, content: commands.clean_content):
-    if not os.path.exists("index"):
-      os.mkdir("index")
-      ix = create_in("index", self.schema)
-      ix = open_dir("index")
-    else:
-      ix = open_dir("index")
-    writer = AsyncWriter(ix)
-    writer.update_document(title=name, content=content, path=name, editor=str(ctx.message.author))
-
-    writer.commit()
+    self.table.insert(dict(title=name, content=content, editor=str(ctx.author)))
+    self.db.commit()
     await ctx.send(f"Created tag {name}")
   
   @commands.command()
   async def show(self, ctx, *, name):
-    if not os.path.exists("index"):
-      os.mkdir("index")
-      ix = create_in('index', self.schema)
-      ix = open_dir('index')
-    else:
-      ix = open_dir("index")
-    with ix.searcher() as searcher:
-      parser = QueryParser('title', schema=ix.schema)
-      query = parser.parse(name)
-      corrected = searcher.correct_query(query, name, maxdist=2)
-      if corrected.query != query:
-        await ctx.send(f"Tag not found. Did you mean: {corrected.string}?")
-      else:
-        results = searcher.search(query)
-        await ctx.send(results[0]['content'])
+    result = self.table.find_one(title=name)
+    await ctx.send(result['content'])
   
   @commands.command()
   async def edit(self, ctx, name, *, content: commands.clean_content):
-    if not os.path.exists("index"):
-      os.mkdir("index")
-      ix = create_in("index", self.schema)
-      ix = open_dir("index")
-    else:
-      ix = open_dir("index")
-    writer = AsyncWriter(ix)
-    writer.update_document(title=name, content=content, path=name, editor=str(ctx.author))
-    writer.commit()
+    self.table.delete(title=name)
+    self.table.insert(dict(title=name, content=content, editor=str(ctx.author)))
+    self.db.commit()
     await ctx.send(f"Edited tag {name}")
 
 
   @commands.command()
   async def taglist(self, ctx):
-    if not os.path.exists("index"):
-      os.mkdir("index")
-      ix = create_in('index', self.schema)
-      ix = open_dir('index')
-    else:
-      ix = open_dir("index")
     x = []
-    with ix.searcher() as searcher:
-      results = searcher.search(Every(), limit=None)
-      i = 0
-      for result in results:
-        i += 1
-        title = result['title']
-        x.append(f"{i}. {title}")
+    i = 0
+    for tag in self.db['tags']:
+      i += 1
+      x.append(f"{i}. {tag['title']}")
     embed = discord.Embed(title="Tag List", description="\n".join(x), colour=0x00ff00)
     await ctx.send(embed=embed)
 
   @commands.command()
   async def delete(self, ctx, *, name):
-    if not os.path.exists("index"):
-      os.mkdir("index")
-      ix = create_in('index', self.schema)
-      ix = open_dir('index')
-    else:
-      ix = open_dir("index")
-    writer = AsyncWriter(ix)
-    writer.delete_by_term('path', name)
-    writer.commit()
+    self.table.delete(title=name)
     await ctx.send(f"Deleted tag {name}")
 
-  @commands.command()
-  async def taginfo(self, ctx, *, name):
-    if not os.path.exists('index'):
-      os.mkdir('index')
-      ix = create_in('index', self.schema)
-      ix = open_dir('index')
-    else:
-      ix = open_dir('index')
-    with ix.searcher() as searcher:
-      parser = QueryParser('title', schema=ix.schema)
-      query = parser.parse(name)
-      corrected = searcher.correct_query(query, name, maxdist=2)
-      if corrected.query != query:
-        await ctx.send(f"Tag not found. Did you mean: {corrected.string}?")
-      else:
-        results = searcher.search(query)
-        title = results[0]['title']
-        editor = results[0]['editor']
-        embed = discord.Embed(title="Tag Info", description="", color=0x00ff00)
-        embed.add_field(name="Title", value=title)
-        embed.add_field(name="Last edited by", value=editor, inline=False)
-        await ctx.send(embed=embed)
-
-  @commands.command()
-  async def tagsearch(self, ctx, *, name):
-    if not os.path.exists('index'):
-      os.mkdir('index')
-      ix = create_in('index', self.schema)
-      ix = open_dir('index')
-    else:
-      ix = open_dir('index')
-    with ix.searcher() as searcher:
-      parser = QueryParser('title', schema=ix.schema)
-      query = parser.parse(name)
-      results = searcher.search(query)
-      corrected = searcher.correct_query(query, name, maxdist=2)
-      if corrected.query != query:
-        await ctx.send(f"Tag not found. Did you mean: {corrected.string}?")
-      else:      
-        x = []
-        i = 0
-        for result in results:
-          i += 1
-          title = result['title']
-          x.append(f"{1}. {title}")
-        y = "\n".join(x)
-        embed = discord.Embed(title=f"Results for {name}:", description=y, color=0x00ff00)
-        await ctx.send(embed=embed)
 
   @commands.command()
   async def raw(self, ctx, *, name):
-    if not os.path.exists('index'):
-      os.mkdir('index')
-      ix = create_in('index', self.schema)
-      ix = open_dir('index')
-    else:
-      ix = open_dir('index')
-    with ix.searcher() as searcher:
-      parser = QueryParser('title', ix.schema)
-      query = parser.parse(name)
-      corrected = searcher.correct_query(query, name, maxdist=2)
-      if corrected.query != query:
-        await ctx.send(f"Tag not found. Did you mean: {corrected.string}?")
-      else:
-        results = searcher.search(query)
-        cleaned = discord.utils.escape_markdown(results[0]['content'])
-        await ctx.send(cleaned)
+    result = self.table.find_one(title=name)
+    cleaned = discord.utils.escape_markdown(result['content'])
+    await ctx.send(cleaned)
 
   @commands.command()
   async def create(self, ctx):
@@ -176,19 +70,11 @@ class Tags(commands.Cog):
       title = msg.content
       await ctx.send(f"Cool! The name is {title}. What about the content? Type your answer in the chat.")
       msg = await self.client.wait_for('message', check=check)
-      if msg.content == f"{ctx.prefix}abort":
+      if msg.content == f'{ctx.prefix}abort':
         await ctx.send("Stopping tag creation.")
       else:
         content = msg.content
-        if not os.path.exists('index'):
-          os.mkdir('index')
-          ix = create_in('index', schema=self.schema)
-          ix = open_dir('index')
-        else:
-          ix = open_dir('index')
-        writer = AsyncWriter(ix)
-        writer.update_document(title=title, content=content, path=title, editor=str(ctx.message.author))
-        writer.commit()
+        self.table.insert(dict(title=title, content=content, editor=str(ctx.author)))
         await ctx.send(f"Created tag {title}")
     
 
