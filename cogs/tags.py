fc69ddd70310dcb80c374cecc01c178ebe08353a
nerdstep710 <49726028+nerdstep710@users.noreply.github.com>
2019-06-23 10:01:06 -0500

    Update tags.py

--- cogs/tags.py
+++ cogs/tags.py
@@ -1,7 +1,5 @@
 import discord
 from discord.ext import commands
-import os.path
-import os
 import sqlite3
 
 
@@ -9,40 +7,49 @@ import sqlite3
 class Tags(commands.Cog):
   def __init__(self, client):
     self.client = client
-    self.con = sqlite3.connect('cogs/data/data.db')
+    self.con = sqlite3.connect('data.db')
     self.cur = self.con.cursor()
     self.cur.execute("CREATE TABLE IF NOT EXISTS tags(title TEXT, content TEXT)")
+    self.con.commit()
 
   
 
 
   @commands.command()
   async def make(self, ctx, name, *, content: commands.clean_content):
+    self.cur.execute("CREATE TABLE IF NOT EXISTS tags(title TEXT, content TEXT)")
     self.cur.execute(f"INSERT INTO tags VALUES('{name}', '{content}')")
+    self.con.commit()
     await ctx.send(f"Created tag {name}")
   
   @commands.command()
   async def show(self, ctx, *, name):
+    self.cur.execute("CREATE TABLE IF NOT EXISTS tags(title TEXT, content TEXT)")
     self.cur.execute(f"SELECT content FROM tags WHERE title = '{name}'")
     result = self.cur.fetchone()
     await ctx.send(result[0])
   
   @commands.command()
   async def edit(self, ctx, name, *, content: commands.clean_content):
+    self.cur.execute("CREATE TABLE IF NOT EXISTS tags(title TEXT, content TEXT)")
     self.cur.execute(f"DELETE FROM tags WHERE title = '{name}'")
     self.cur.execute(f"INSERT INTO tags VALUES('{name}', '{content}')")
+    self.con.commit()
     await ctx.send(f"Edited tag {name}")
 
 
   @commands.command()
   async def delete(self, ctx, *, name):
+    self.cur.execute("CREATE TABLE IF NOT EXISTS tags(title TEXT, content TEXT)")
     self.cur.execute(f"DELETE FROM tags WHERE title = '{name}'")
+    self.con.commit()
     await ctx.send(f"Deleted tag {name}")
 
 
 
   @commands.command()
   async def raw(self, ctx, *, name):
+    self.cur.execute("CREATE TABLE IF NOT EXISTS tags(title TEXT, content TEXT)")
     self.cur.execute(f"SELECT content FROM tags WHERE title = '{name}'")
     result = self.cur.fetchone()
     cleaned = discord.utils.escape_markdown(result[0])
@@ -50,6 +57,7 @@ class Tags(commands.Cog):
 
   @commands.command()
   async def create(self, ctx):
+    self.cur.execute("CREATE TABLE IF NOT EXISTS tags(title TEXT, content TEXT)")
     await ctx.send(f"Hey! So I heard you want to make a tag. What's it gonna be called? Type your answer in the chat, or type {ctx.prefix}abort at any to stop making a tag.")
     def check(m):
       return m.channel == ctx.message.channel and m.author == ctx.message.author
@@ -65,12 +73,20 @@ class Tags(commands.Cog):
       else:
         content = msg.content
         self.cur.execute(f"INSERT INTO tags VALUES('{title}', '{content}')")
+        self.con.commit()
         await ctx.send(f"Created tag {title}")
 
   @commands.command()
   async def taglist(self, ctx):
-    result = [job[0] for job in self.cur.execute("SELECT title FROM tags")]
-    embed = discord.Embed(title="Tag List", description="\n".join(result), color=)
+    self.cur.execute("CREATE TABLE IF NOT EXISTS tags(title TEXT, content TEXT)")
+    results = [job[0] for job in self.cur.execute("SELECT title FROM tags")]
+    x = []
+    i = 0
+    for result in results:
+      i += 1
+      x.append(f"{i}. {result}")
+    embed = discord.Embed(title="Tag List", description="\n".join(x))
+    await ctx.send(embed=embed)
     
 
 def setup(client):
