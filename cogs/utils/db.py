5ba918f1a2253cdfbf90d387418a693d8f91df52
nerdstep710 <49726028+nerdstep710@users.noreply.github.com>
2019-06-28 09:17:45 -0500

    Update db.py

--- cogs/utils/db.py
+++ cogs/utils/db.py
@@ -1,35 +1,31 @@
-import aiosqlite
+import sqlite3
 
-async def make_tag(name, content):
-  async with aiosqlite.connect('data.db') as con:
-    await con.execute("CREATE TABLE IF NOT EXISTS tags(title TEXT, content TEXT)")
-    await con.execute(f"INSERT INTO tags VALUES(?, ?)", (name.lower(), content))
-    await con.commit()
-    print("done")
 
-async def get_tag(name):
-  async with aiosqlite.connect('data.db') as con:
-    await con.execute("CREATE TABLE IF NOT EXISTS tags(title TEXT, content TEXT)")
-    async with con.execute("SELECT content FROM tags WHERE title=?", [name.lower()]) as cur:
-      result = await cur.fetchone()
-      return result[0]
-      print("done")
 
-async def delete_tag(name):
-  async with aiosqlite.connect('data.db') as con:
-    await con.execute("CREATE TABLE IF NOT EXISTS tags (title TEXT, content TEXT)")
-    await con.execute("DELETE FROM tags WHERE title=?", [name.lower()])
-    await con.commit()
-    print("done")
-
-class dbOps:
+class db:
   def __init__(self):
-    print("Ready!")
-
+    self.con = sqlite3.connect('data.db')
+    self.cur = self.con.cursor()
+  
   async def make_table(self):
-    async with aiosqlite.connect('data.db') as con:
-      await con.execute
-
+    self.cur.execute("CREATE TABLE IF NOT EXISTS tags (title TEXT, content TEXT)")
+    self.con.commit()
+    print('done')
+  
   async def make_tag(self, name, content):
-    async with aiosqlite.connect('data.db') as con:
-      await con.execute("INSERT INTO tags VALUES(?, ?)", (name.lower(), content))
+    await self.make_table()
+    self.cur.execute("INSERT INTO tags VALUES(?, ?)", (name.lower(), content))
+    self.con.commit()
+    print('done')
+  
+  async def get_tag(self, name):
+    await self.make_table()
+    self.cur.execute("SELECT content FROM tags WHERE title=?", [name.lower()])
+    result = self.cur.fetchone()
+    return result[0]
+    print('done')
+  
+  async def delete_tag(self, name):
+    await self.make_table()
+    self.cur.execute("DELETE FROM tags WHERE title=?", [name.lower()])
+    self.con.commit()
