5d75e7bf744f90cfa2cb7343d0dcb63855895d30
nerdstep710 <49726028+nerdstep710@users.noreply.github.com>
2019-07-11 23:21:39 -0500

    Reflect error handler update

--- cogs/math.py
+++ cogs/math.py
@@ -9,32 +9,22 @@ class MathCog(commands.Cog):
     self.client = client
 
   @commands.command()
-  async def square(self, ctx, number=None):
-	  if number is None:
-	  	await ctx.send("You must provide a number to square.")
+  async def square(self, ctx, number):
 	  squared_value = float(number) * float(number)
 	  await ctx.send(f"{number}² is {squared_value}")
 
   @commands.command()
-  async def root(self, ctx, number=None):
-	  if number is None:
-	  	await ctx.send("You must provide a number.")
-	  	return
+  async def root(self, ctx, number):
 	  answer = math.sqrt(float(number))
 	  await ctx.send(f"The square root of {number} is {answer}")
   
   @commands.command()
-  async def multiply(self, ctx, number1=None, number2=None):
-	  if number1 is None or number2 is None:
-	  	await ctx.send("You must provide two numbers to multiply, separated by a space.")
+  async def multiply(self, ctx, number1, number2):
 	  multiplied_value = float(number1) * float(number2)
 	  await ctx.send(f"{number1} multiplied by {number2} is {multiplied_value}")
   
   @commands.command(aliases=["fact"])
-  async def factorial(self, ctx, number=None):
-    if number is None:
-      await ctx.send("You must provide a number!")
-      return
+  async def factorial(self, ctx, number):
     number = int(number)
     if number > 802:
       await ctx.send("Sorry, but your number is too big! We don't want Discord getting mad at us, after all.")
@@ -43,57 +33,37 @@ class MathCog(commands.Cog):
     await ctx.send(f"{number} factorial is {answer}")
 
   @commands.command()
-  async def exp(self, ctx, number1=None, number2=None):
-    if number1 is None or number2 is None:
-      await ctx.send("You must provide two numbers, separated with a space.")
+  async def exp(self, ctx, number1, number2):
     answer = float(number1) ** float(number2)
     await ctx.send(f"{number1} to the power of {number2} is {answer}")
 
   @commands.command()
-  async def sine(self, ctx, number=None):
-    if number is None:
-      await ctx.send("You must provide a number!")
-      return
+  async def sine(self, ctx, number):
     answer = math.sin(float(number))
     await ctx.send(f"The sine of {number} is {answer}")
 
   @commands.command()
-  async def cos(self, ctx, number=None):
-    if number is None:
-      await ctx.send("You must provide a number!")
-      return
+  async def cos(self, ctx, number):
     answer = math.cos(float(number))
     await ctx.send(f"The cosine of {number} is {answer}")
 
   @commands.command()
-  async def tan(self, ctx, number=None):
-    if number is None:
-      await ctx.send("You must provide a number!")
-      return
+  async def tan(self, ctx, number):
     answer = math.tan(float(number))
     await ctx.send(f"The tangent of {number} is {answer}")
 
   @commands.command()
-  async def divide(self, ctx, number1=None, number2=None):
-    if number1 is None or number2 is None:
-      await ctx.send("You must provide two numbers to divide, separated by a space.")
-      return  
+  async def divide(self, ctx, number1, number2):
     answer = float(number1) / float(number2)
     await ctx.send(f"{number1} divided by {number2} is {answer}")
 
   @commands.command()
-  async def add(self, ctx, number1=None, number2=None):
-	  if number1 is None or number2 is None:
-	  	await ctx.send("You must provide two numbers to add, separated by a space.")
-	  	return 
+  async def add(self, ctx, number1, number2):
 	  added_value = float(number1) + float(number2)
 	  await ctx.send(f"{number1} + {number2} is {added_value}")
 
   @commands.command()
-  async def subtract(self, ctx, number1=None, number2=None):
-	  if number1 is None or number2 is None:
-		  await ctx.send("You must provide two numbers to subtract, separated by a space.")
-		  return  
+  async def subtract(self, ctx, number1, number2):
 	  subtracted_value = float(number1) - float(number2)
 	  await ctx.send(f"{number1} - {number2} is {subtracted_value}")
 
