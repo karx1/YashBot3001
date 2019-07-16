911bab1719af51388dcb845ab45aceac1d8865fa
nerdstep710 <49726028+nerdstep710@users.noreply.github.com>
2019-07-16 14:31:36 -0500

    Subclass commands.Bot and move events to subclass

--- bot.py
+++ bot.py
@@ -2,18 +2,36 @@ import discord
 from discord.ext import commands
 import typing
 import os
+import asyncio
+
+class customBetaBot(commands.Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.load_extension("jishaku")
+
+    async def on_ready(self):
+        print(f"Logged in as: {client.user}")
+        print(f"discord.py  version: {discord.__version__}")
+    
+    async def on_command_error(self, ctx, error):
+        error_str = str(error)
+        error = getattr(error, 'original', error)
+        permcheck = (commands.BotMissingPermissions, commands.MissingPermissions, discord.Forbidden)
+        if isinstance(error, commands.CommandNotFound):
+            return
+        elif isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send(f"Looks like you forgot to provide `{error.param}`!")
+        elif isinstance(error, permcheck):
+            await ctx.send(f"Looks like I am forbidden from performing this command here.")
+        else:
+            await ctx.send(f"Error: {error_str}")
+
+client = customBetaBot(case_insensitive=True, command_prefix=":")
 
 
 
-client = commands.Bot(case_insensitive=True, command_prefix=":")
 
-
-
-@client.event
-async def on_ready():
-    client.load_extension("jishaku")
-    print(f"Logged in as: {client.user}")
-    print(f"discord.py version: {discord.__version__}")
 
 @client.command()
 async def bot(ctx, member: typing.Union[discord.Member, discord.User], *, message: str):
@@ -25,22 +43,10 @@ async def bot(ctx, member: typing.Union[discord.Member, discord.User], *, messag
 @client.command(hidden=True)
 @commands.is_owner()
 async def shutdown(ctx):
-    await ctx.send("Shutting Down")
+    await ctx.send("Shutting Down...")
+    await asyncio.sleep(5)
+    await ctx.send("Sucessfully shut down beta bot.")
     await client.close()
-
-@client.event
-async def on_command_error(ctx, error):
-    error_str = str(error)
-    error = getattr(error, 'original', error)
-    permcheck = (commands.BotMissingPermissions, commands.MissingPermissions, discord.Forbidden)
-    if isinstance(error, commands.CommandNotFound):
-        return
-    elif isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send(f"Looks like you forgot to provide `{error.param}`!")
-    elif isinstance(error, permcheck):
-        await ctx.send(f"Looks like I am forbidden from performing this command here.")
-    else:
-        await ctx.send(f"Error: {error_str}")
 
 
 token = open("./token.txt", 'r').read()
