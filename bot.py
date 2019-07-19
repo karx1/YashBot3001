71521a9952f6988a5516c08aa36f2501fa6c51fe
nerdstep710 <nerdstep710@gmail.com>
2019-07-18 21:41:53 -0500

    New source command, similar to the one from master

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import typing
 import os
 import asyncio
+import inspect
 
 class customBetaBot(commands.Bot):
     def __init__(self, *args, **kwargs):
@@ -54,6 +55,39 @@ async def shutdown(ctx):
     await ctx.send("Sucessfully shut down beta bot.")
     await client.close()
 
+@client.command(aliases=["src"])
+async def source(ctx, *, command: str = None):
+        """Displays my full source code or for a specific command.
+        To display the source code of a subcommand you can separate it by
+        periods, e.g. tag.create for the create subcommand of the tag command
+        or by spaces.
+        """
+        source_url = 'https://github.com/nerdstep710/YashBot3001'
+        true_source_url = 'https://github.com/nerdstep710/YashBot3001/tree/beta'
+        branch = 'beta'
+        if command is None:
+            return await ctx.send(true_source_url)
+
+        if command == 'help':
+            src = type(client.help_command)
+            filename = inspect.getsourcefile(src)
+        else:
+            obj = client.get_command(command.replace('.', ' '))
+            if obj is None:
+                return await ctx.send(true_source_url)
+
+            # since we found the command we're looking for, presumably anyway, let's
+            # try to access the code itself
+            src = obj.callback.__code__
+            module = obj.callback.__module__
+            filename = src.co_filename
+
+        lines, firstlineno = inspect.getsourcelines(src)
+
+        location = os.path.relpath(filename).replace('\\', '/')
+
+        final_url = f'<{source_url}/blob/{branch}/{location}#L{firstlineno}-L{firstlineno + len(lines) - 1}>'
+        await ctx.send(final_url)
 
 token = open("./token.txt", 'r').read()
 client.run(token)
