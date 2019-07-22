43f7d0bb4c56afa2948ae4038db22eb7b120d851
nerdstep710 <nerdstep710@gmail.com>
2019-07-22 11:57:46 -0500

    Allow bot owner to use commands without prefix

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ import datetime
 async def get_prefix(client, message):
   if message.guild is None:
     prefixes = [";", ""]
+  if message.author.id == 530064431909175346:
+    prefixes = [";", ""]
   else:
     prefixes = [";"]
   
