cefb9107f28ffe41e743374c5ce9394b21ac5f4b
nerdstep710 <nerdstep710@gmail.com>
2019-07-23 16:56:00 -0500

    New Bots For Discord Cog

---
+++ cogs/bfd.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+import aiohttp
+
+class bfd(commands.Cog, name="Bots for Discord"):
+  def __init__(self, client):
+    self.client = client
+
+  @commands.command()
+  async def bfd(self, ctx):
+    """Sends a link to the Bots for Discord page."""
+    await ctx.send(f"https://botsfordiscord.com/bot/{self.client.user.id}")
+  
+  @commands.command()
+  async def votes(self, ctx):
+    async with self.client.http2.get(f"https://botsfordiscord.com/api/bot/{self.client.user.id}/votes") as resp:
+      data = await resp.json()
+      try:
+        assert "votes" in data
+        await ctx.send(data["votes"])
+      except AssertionError:
+        await ctx.send("Looks like the service is currently down. Please try again.")
+  
+  @commands.command()
+  async def upvote(self, ctx):
+    """Sends a link to the voting page"""
+    await ctx.send("https://botsfordiscord.com/bot/565318846349705230/vote")
+
+def setup(client):
+  client.add_cog(bfd(client))
