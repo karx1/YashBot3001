0516f527e25af43a78de8bb7cc9724cbca8504a9
Yash Karandikar <nerdstep710@gmail.com>
2020-06-13 16:05:49 -0500

    Post all math answers to mystbin

--- cogs/math.py
+++ cogs/math.py
@@ -17,6 +17,23 @@ def factorial(ctx, n):
         return f
 
 
+async def check_answer(ctx, answer):
+    print(str(answer))
+    if len(str(answer)) > 2000:
+        try:
+            url = await ctx.bot.post_to_mystbin(answer)
+            await ctx.send(f"Your result was too long for discord, so I put it here instead! {url}")
+        except (aiohttp.ContentTypeError, AssertionError):
+            fp = discord.File(BytesIO(str(answer).encode("utf-8")), "out.txt")
+            await ctx.send("Your file was too long for discord, so I put it here instead!", file=fp)
+            return None
+    elif str(answer) == "inf":
+        await ctx.send("This number is too large for Python to handle!")
+        return None
+    else:
+        return True
+
+
 class Math(commands.Cog):
     def __init__(self, client):
         self.client = client
@@ -24,18 +41,21 @@ class Math(commands.Cog):
     @commands.command()
     async def square(self, ctx, number):
         square = lambda x: float(x) ** 2
-        await ctx.send(f"{number}² is {square(number)}")
+        if await check_answer(ctx, square):
+            await ctx.send(f"{number}² is {square(number)}")
 
     @commands.command()
     async def root(self, ctx, number):
         """Shows the square root of a number"""
         answer = math.sqrt(float(number))
-        await ctx.send(f"The square root of {number} is {answer}")
+        if await check_answer(ctx, answer):
+            await ctx.send(f"The square root of {number} is {answer}")
 
     @commands.command()
     async def multiply(self, ctx, number1, number2):
         multiplied_value = float(number1) * float(number2)
-        await ctx.send(f"{number1} multiplied by {number2} is {multiplied_value}")
+        if await check_answer(ctx, multiplied_value):
+            await ctx.send(f"{number1} multiplied by {number2} is {multiplied_value}")
 
     @commands.command()
     @commands.cooldown(1, 15, BucketType.default)
@@ -46,59 +66,56 @@ class Math(commands.Cog):
             )
             return
         answer = await factorial(ctx, number)
-        if len(str(answer)) > 2000:
-            try:
-                url = await ctx.bot.post_to_mystbin(answer)
-                await ctx.send(
-                    f"Your result was too long for discord, so I put it here instead! {url}"
-                )
-            except (aiohttp.ContentTypeError, AssertionError):
-                fp = discord.File(BytesIO(str(answer).encode("utf-8")), "out.txt")
-                await ctx.send(
-                    "Your result was too long for discord, so I put it here instead!",
-                    file=fp,
-                )
-        else:
+        if await check_answer(ctx, answer):
             await ctx.send(answer)
 
     @commands.command()
     async def exp(self, ctx, number1, number2):
         answer = float(number1) ** float(number2)
-        await ctx.send(f"{number1} to the power of {number2} is {answer}")
+        if await check_answer(ctx, answer):
+            await ctx.send(f"{number1} to the power of {number2} is {answer}")
 
     @commands.command()
     async def sine(self, ctx, number):
         answer = math.sin(float(number))
-        await ctx.send(f"The sine of {number} is {answer}")
+        if await check_answer(ctx, answer):
+            await ctx.send(f"The sine of {number} is {answer}")
 
     @commands.command()
     async def cos(self, ctx, number):
         answer = math.cos(float(number))
-        await ctx.send(f"The cosine of {number} is {answer}")
+        if await check_answer(ctx, answer):
+            await ctx.send(f"The cosine of {number} is {answer}")
 
     @commands.command()
     async def tan(self, ctx, number):
         answer = math.tan(float(number))
-        await ctx.send(f"The tangent of {number} is {answer}")
+        if await check_answer(ctx, answer):
+            await ctx.send(f"The tangent of {number} is {answer}")
 
     @commands.command()
     async def divide(self, ctx, number1, number2):
         answer = float(number1) / float(number2)
-        await ctx.send(f"{number1} divided by {number2} is {answer}")
+        if await check_answer(ctx, answer):
+            await ctx.send(f"{number1} divided by {number2} is {answer}")
 
     @commands.command()
     async def add(self, ctx, number1, number2):
         added_value = float(number1) + float(number2)
-        await ctx.send(f"{number1} + {number2} is {added_value}")
+        if await check_answer(ctx, added_value):
+            await ctx.send(f"{number1} + {number2} is {added_value}")
 
     @commands.command()
     async def subtract(self, ctx, number1, number2):
         subtracted_value = float(number1) - float(number2)
-        await ctx.send(f"{number1} - {number2} is {subtracted_value}")
+        if await check_answer(ctx, subtracted_value):
+            await ctx.send(f"{number1} - {number2} is {subtracted_value}")
 
     @commands.command()
     async def average(self, ctx, *args: int):
-        await ctx.send(f"The average is {sum(args) / len(args)}")
+        answer = sum(args) / len(args)
+        if await check_answer(ctx, answer):
+            await ctx.send(f"The average is {answer}")
 
 
 def setup(client):
